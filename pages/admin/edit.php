3801ed8f96b976549010739b7221b1429a6a1c01
Aoniq <89135@glr.nl>
2023-09-05 11:04:33 +0200

    Last touches

--- pages/admin/edit.php
+++ pages/admin/edit.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include_once('../../assets/modules/mysql.php');
-
+session_start();
 $projectId = (int)$_GET['id']; // Sanitize and cast project ID to an integer
 
 // Count the number of images associated with the project
@@ -14,15 +14,23 @@ if ($countRow = mysqli_fetch_assoc($countResult)) {
     $imageCount = (int)$countRow['imageCount']; // Get the image count
 }
 
-$query = "SELECT p.ProjectName, p.ProjectID, p.creation_date, p.description, p.github_url, p.demo_url, i.ImageURL
-FROM Projects p
-LEFT JOIN Images i ON p.ProjectID = i.ProjectID WHERE p.ProjectID = $projectId LIMIT $imageCount;";
+$query = "SELECT p.ProjectName, p.ProjectID, p.creation_date, p.description, p.github_url, p.demo_url
+FROM Projects p WHERE p.ProjectID = $projectId;";
 
 $result = mysqli_query($connection, $query);
 
 $project = null; // Initialize the $project variable
 
+// make query for images based on project id
+$ImgQuery = "SELECT i.ImageURL, i.ImageID FROM Images i WHERE i.ProjectID = $projectId;";
+$Imgresult = mysqli_query($connection, $ImgQuery);
+
 $images = []; // Initialize the $images array to store project images
+
+// store images in array
+while ($row = mysqli_fetch_assoc($Imgresult)) {
+    $images[] = $row;
+}
 
 while ($row = mysqli_fetch_assoc($result)) {
     // Populate the $project array with project details once
@@ -36,33 +44,119 @@ while ($row = mysqli_fetch_assoc($result)) {
             'demo_url' => $row['demo_url'],
         ];
     }
-
-    // Add the image URL to the $images array
-    if (!empty($row['ImageURL'])) {
-        $images[] = $row['ImageURL'];
-    }
 }
 
 mysqli_free_result($result);
 
-// Separate query to retrieve language names
-$languageNames = []; // Initialize the $languageNames array to store language names
-
-$languageQuery = "SELECT l.LanguageName
+// get languages for the project
+$ProjectLanguageQuery = "SELECT l.LanguageName, l.LanguageID
 FROM Projects p
 LEFT JOIN ProjectLanguages pl ON p.ProjectID = pl.ProjectID
 LEFT JOIN Languages l ON pl.LanguageID = l.LanguageID
 WHERE p.ProjectID = $projectId;";
-
-$languageResult = mysqli_query($connection, $languageQuery);
-
-while ($languageRow = mysqli_fetch_assoc($languageResult)) {
+$ProjectLanguageResult = mysqli_query($connection, $ProjectLanguageQuery);
+$ProjectLanguageNames = []; // Initialize the $languageNames array to store language names
+while ($ProjectLanguageRow = mysqli_fetch_assoc($ProjectLanguageResult)) {
     // Add language names to the $languageNames array
-    $languageNames[] = $languageRow['LanguageName'];
+    $ProjectLanguageNames[] = $ProjectLanguageRow['LanguageName'];
+}
+mysqli_free_result($ProjectLanguageResult);
+
+// get all languages
+$languageQuery = "SELECT * FROM Languages;";
+$languageResult = mysqli_query($connection, $languageQuery);
+$languages = []; // Initialize the $languages array to store language s
+while ($languageRow = mysqli_fetch_assoc($languageResult)) {
+    // Add language s to the $languages array
+    $languages[] = $languageRow;
 }
 
 mysqli_free_result($languageResult);
 
+if ($_SESSION['loggedin'] == true && isset($_GET['id']) && isset($_GET['image'])) {
+    $id = $_GET['id'];
+    $imageId = $_GET['image'];
+    $query = "DELETE FROM Images WHERE ImageID = $imageId;";
+    if (!mysqli_query($connection, $query)) {
+        echo mysqli_error($connection);
+    }
+    header("Location: edit.php?id=$id");
+}
+
+if ($_SESSION['loggedin'] == true && isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    // trim description
+    $description = trim($_POST['description']);
+    $githubURL = $_POST['githubURL'];
+    $demoURL = $_POST['demoURL'];
+    $languages = $_POST['languages'];
+    $query = "UPDATE Projects SET ProjectName = '$name', description = '$description', github_url = '$githubURL', demo_url = '$demoURL' WHERE ProjectID = $projectId;";
+    if (!mysqli_query($connection, $query)) {
+        echo mysqli_error($connection);
+    }
+    // update languages
+    // check which languages are checked and unchecked
+    $languageQuery = "SELECT * FROM ProjectLanguages WHERE ProjectID = $projectId;";
+    $languageResult = mysqli_query($connection, $languageQuery);
+    $checkedLanguages = []; // Initialize the $checkedLanguages array to store checked languages
+    while ($languageRow = mysqli_fetch_assoc($languageResult)) {
+        // Add checked languages to the $checkedLanguages array
+        $checkedLanguages[] = $languageRow['LanguageID'];
+    }
+    mysqli_free_result($languageResult);
+    // update languages
+    foreach ($checkedLanguages as $language) {
+        if (!in_array($language, $languages)) {
+            $query = "DELETE FROM ProjectLanguages WHERE ProjectID = $projectId AND LanguageID = $language;";
+            if (!mysqli_query($connection, $query)) {
+                echo mysqli_error($connection);
+            }
+        }
+    }
+    foreach ($languages as $language) {
+        if (!in_array($language, $checkedLanguages)) {
+            $query = "INSERT INTO ProjectLanguages (ProjectID, LanguageID) VALUES ($projectId, $language);";
+            if (!mysqli_query($connection, $query)) {
+                echo mysqli_error($connection);
+            }
+        }
+    }
+
+    // check if images is uploaded
+    if (!empty($_FILES['media']['name'])) {
+        // upload image
+        $mediaFiles = $_FILES['media'];
+        $mediaCount = count($mediaFiles['name']);
+        // make foreach for uploading all images
+        for ($i = 0; $i < $mediaCount; $i++) {
+            $mediaName = $mediaFiles['name'][$i];
+            $mediaTmpName = $mediaFiles['tmp_name'][$i];
+            $mediaType = $mediaFiles['type'][$i];
+
+            // Process and save the media file to a directory
+            $targetDir = '../../assets/uploads/';
+            $targetPath = $targetDir . $mediaName;
+            if (move_uploaded_file($mediaTmpName, $targetPath)) {
+                // File uploaded successfully
+            } else {
+                // Error uploading file
+                echo "Error uploading file: " . $_FILES['media']['error'][$i];
+            }
+
+            // Save media information to the database
+            // You need to write the appropriate code to insert the data into your database
+            // use the last inserted id to insert media
+            $stmt = $connection->prepare("INSERT INTO Images (ProjectID, imageURL) VALUES (?, ?)");
+            $stmt->bind_param("is", $projectId, $mediaName);
+            $stmt->execute();
+        
+        }
+    }
+
+    header("Location: edit.php?id=$projectId");
+}
+
 // Include your view file
 include_once("edit_view.php");
+
 ?>
--- pages/admin/edit_view.php
+++ pages/admin/edit_view.php
@@ -43,7 +43,7 @@
         </div>
     </div>
 
-    <div class="flex justify-center bg-black items-center">
+    <div class="flex justify-center bg-black items-center pb-24">
         <div class="container mt-48">
             <div class="bg-zinc-900 bg-opacity-50 rounded-2xl p-8 shadow lg:w-1/2 w-full mx-auto justify-center">
                 <h2 class="text-2xl text-slate-100 font-semibold mb-4">Edit <?= $project['ProjectName'] ?></h2>
@@ -66,34 +66,60 @@
                         <label for="demoURL">Demo URL</label>
                         <input type="text" name="demoURL" value="<?= $project['demo_url'] ?>" class="input input-bordered w-full bg-opacity-25" />
                     </div>
-                    <label for="">Languages Used</label>
                     <div class="mt-4">
+                        <label for="">Languages Used</label>
                         <!-- foreach add all languages  -->
-                            <?php
-
-                            foreach ($languages as $language) {
-                                echo '<div class="form-control">';
-                                echo '<label class="label cursor-pointer">';
-                                echo '<span class="label-text">' . $language['LanguageName'] . '</span>';
-                                echo '<input type="checkbox" class="checkbox checkbox-info" name="languages[]" value="' . $language['LanguageID'] . '" />';
-                                echo '</label>';
-                                echo '</div>';
-                            
+                        <?php
+                        foreach ($languages as $language) {
+                            echo '<div class="form-control">';
+                            echo '<label class="label cursor-pointer">';
+                            echo '<span class="label-text">' . $language['LanguageName'] . '</span>';
+                            // Assuming $language is a string, not an array
+                            echo '<input type="checkbox" class="checkbox checkbox-info" name="languages[]" value="' . $language['LanguageID'] . '" ';
+                            // Check if the language is in the $projectLanguages array and add 'checked' attribute
+                            if (in_array($language['LanguageName'], $ProjectLanguageNames)) {
+                                echo 'checked';
                             }
-                            ?>
+                            echo ' />';
+                            echo '</label>';
+                            echo '</div>';
+                        }
+                        ?>
+
                     </div>
                     <div class="mt-4">
-                        <label for="demoURL">Media</label>
-                        <div id="mediaContainer" class="">
+                        <div id="mediaContainer" class="mt-4">
                             <input type="file" name="media[]" class="file-input file-input-bordered w-full bg-opacity-25 mb-2" />
+                            <button type="button" id="addMediaButton">Add Media</button>
                         </div>
-                        <button type="button" id="addMediaButton">Add Media</button>
                     </div>
                     <div class="mt-4">
-                        <button name="submit" class="btn bg-gradient-to-r from-blue-500 to-blue-400 text-white transition duration-300 ease-in-out hover:shadow-lg">Create</button>
+                        <button name="submit" class="btn bg-gradient-to-r from-blue-500 to-blue-400 text-white transition duration-300 ease-in-out hover:shadow-lg">Save changes</button>
                     </div>
                 </form>
+                <div class="mt-4">
+                    <label for="demoURL">Media</label>
+                    <div class="">
+                        <table class="table table-zebra w-full">
+                            <tr>
+                                <th>Image</th>
+                                <th class="text-end">Actions</th>
+                            </tr>
 
+                            <?php
+                            foreach ($images as $image) {
+                                // make table row with image and actions buttons
+                                echo '<tr>';
+                                echo '<td><img src="../../assets/uploads/' . $image['ImageURL'] . '" alt="" width="100" height="100"></td>';
+                                echo '<td class="text-end">';
+                                echo '<a href="edit.php?id=' . $project['ProjectID'] . '&image=' . $image['ImageID'] . '" class="btn btn-sm btn-error text-white">Delete</button>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
+                            ?>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
 
